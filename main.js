3b1e0d94e8c779b26d23b97b96208c18d9d889d7
sahaj-67 <bansalsavita649@gmail.com>
2025-02-01 10:02:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let img = document.getElementsByTagName('h3');
+
+img[0].addEventListener('click' , function(){
+    open("./air_polution.html" , '_parent')
+});
+img[1].addEventListener('click' , function(){
+    open("./water.html" , '_parent')
+});
+img[2].addEventListener('click' , function(){
+    open("./land.html" , '_parent')
+});
+img[3].addEventListener('click' , function(){
+    open("./noice.html" , '_parent')
+});
